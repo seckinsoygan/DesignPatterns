7311717dcebd0e961ee1e0121cd71e4ccfd823c5
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-08-17 02:02:54 +0300

    Add project files.

---
+++ ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/ChainOfResponsibility/Treasurer.cs
@@ -0,0 +1,36 @@
+﻿using DesignPattern.ChainOfResponsibility.DataAccess;
+using DesignPattern.ChainOfResponsibility.Models;
+
+namespace DesignPattern.ChainOfResponsibility.ChainOfResponsibility
+{
+    public class Treasurer : Employee
+    {
+
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            AppDbContext context = new AppDbContext();
+            if (req.Amount <= 100000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Veznedar - Ayşe Çınar";
+                customerProcess.Description = "Para çekme işlemi onaylandı. Müşterinin talep ettiği tutar ödendi.";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            else if (NextApprover != null)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Veznedar - Ayşe Çınar";
+                customerProcess.Description = "Para çekme tutarı veznedarın ödeyeceği limiti aştığı için , işlem şube müdür yardımcısına yönlendirildi. ";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+                NextApprover.ProcessRequest(req);
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/DataAccess/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.ChainOfResponsibility.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DesignPattern1;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
